b9b5fdf485dde8bb3b0c047c0adf6037b5e3bac8
Markiyan Tymkevych <tmb0208@gmail.com>
2019-11-03 14:52:58 +0200

    Refactor and format

--- build_cpp_class_uml_representation.py
+++ build_cpp_class_uml_representation.py
@@ -18,38 +18,34 @@ def parse_args():
     return args
 
 
-def build_uml_properties_representation(cpp_class, access_modificators, access_modificator_representations):
+def build_uml_properties_representation(cpp_class, access_modificator_representations):
     results = []
 
     uml_property_representation = "{} {} : {}"
 
     properties = cpp_class["properties"]
-    for access_modificator in access_modificators:
-        for property in properties[access_modificator]:
-            result = uml_property_representation.format(
-                access_modificator_representations[access_modificator], property["name"], property["type"])
-
-            # if property["static"]:
-            #     result = "<u>{}</u>".format(result)
-
-            results.append(result)
+    for acc_mod, acc_mod_rep in access_modificator_representations.items():
+        for property in properties[acc_mod]:
+            results.append(uml_property_representation.format(
+                acc_mod_rep, property["name"], property["type"]))
 
     return results
 
 
-def build_uml_methods_representation(cpp_class, access_modificators, access_modificator_representations):
+def build_uml_methods_representation(cpp_class, access_modificator_representations):
     results = []
 
     uml_method_representation = "{} {}( {} ): {} {}"
     uml_method_parameter_representation = "{} : {}"
 
     methods = cpp_class["methods"]
-    for access_modificator in access_modificators:
-        for method in methods[access_modificator]:
+    for acc_mod, acc_mod_rep in access_modificator_representations.items():
+        for method in methods[acc_mod]:
             parameters_representation = []
             for parameter in method["parameters"]:
                 parameters_representation.append(
-                    uml_method_parameter_representation.format(parameter["name"], parameter["type"]))
+                    uml_method_parameter_representation.format(parameter["name"],
+                                                               parameter["type"]))
 
             methodName = method["name"]
             returnType = ""
@@ -66,7 +62,7 @@ def build_uml_methods_representation(cpp_class, access_modificators, access_modi
             elif method["override"]:
                 specificators = "[override]"
 
-            result = uml_method_representation.format(access_modificator_representations[access_modificator],
+            result = uml_method_representation.format(acc_mod_rep,
                                                       methodName,
                                                       ', '.join(parameters_representation),
                                                       returnType,
@@ -97,13 +93,41 @@ def italic_if_pure_virtual(uml_representation):
     return uml_representation
 
 
-def format_method_if_too_long(uml_method_representation, max_chars, spacing=8):
-    spacing_string = " " * spacing
-    html_new_line = "<br />"
+def format_if_too_long(uml_method_representation, max_chars, spacing=8):
+    sep = "<br />" + (" " * spacing)
+
     if len(uml_method_representation) > max_chars:
-        return uml_method_representation.replace("( ", "(" + html_new_line + spacing_string).replace(", ", "," + html_new_line + spacing_string)
+        return uml_method_representation.replace("( ", "(" + sep).replace(", ", "," + sep)
 
     return uml_method_representation
+
+
+def replace_html_specific_characters(string):
+    return string.replace("&", "&#38;").replace("<", "&#60;").replace(">", "&#62;")
+
+
+def build_html_uml_properties_representation(uml_properties_representation):
+    results = []
+
+    for r in uml_properties_representation:
+        r = replace_html_specific_characters(r)
+        r = underline_if_static(r)
+        results.append(r)
+
+    return results
+
+
+def build_html_uml_methods_representation(uml_methods_representation):
+    results = []
+
+    for r in uml_methods_representation:
+        r = replace_html_specific_characters(r)
+        r = underline_if_static(r)
+        r = italic_if_pure_virtual(r)
+        r = format_if_too_long(r, 100)
+        results.append(r)
+
+    return results
 
 
 def build_uml_class_representation(path_to_header, class_name):
@@ -116,27 +140,18 @@ def build_uml_class_representation(path_to_header, class_name):
     cpp_class = cppHeader.classes[class_name]
     class_caption = "{}::{}".format(cpp_class["namespace"], cpp_class["name"])
 
-    access_modificators = ["private", "protected", "public"]
     access_modificator_representations = {"private": "-", "protected": "#", "public": "+"}
 
     uml_properties_representation = build_uml_properties_representation(
-        cpp_class, access_modificators, access_modificator_representations)
+        cpp_class, access_modificator_representations)
 
     uml_methods_representation = build_uml_methods_representation(
-        cpp_class, access_modificators, access_modificator_representations)
+        cpp_class, access_modificator_representations)
 
-    uml_properties_representation = [r.replace("&", "&#38;") for r in uml_properties_representation]
-    uml_properties_representation = [r.replace("<", "&#60;") for r in uml_properties_representation]
-    uml_properties_representation = [r.replace(">", "&#62;") for r in uml_properties_representation]
-    uml_properties_representation = [underline_if_static(r) for r in uml_properties_representation]
-
-    uml_methods_representation = [r.replace("&", "&#38;") for r in uml_methods_representation]
-    uml_methods_representation = [r.replace("<", "&#60;") for r in uml_methods_representation]
-    uml_methods_representation = [r.replace(">", "&#62;") for r in uml_methods_representation]
-    uml_methods_representation = [underline_if_static(r) for r in uml_methods_representation]
-    uml_methods_representation = [italic_if_pure_virtual(r) for r in uml_methods_representation]
-    uml_methods_representation = [format_method_if_too_long(
-        r, 100) for r in uml_methods_representation]
+    html_uml_properties_representation = build_html_uml_properties_representation(
+        uml_properties_representation)
+    html_uml_methods_representation = build_html_uml_methods_representation(
+        uml_methods_representation)
 
     node_template = """
     [
@@ -160,9 +175,10 @@ def build_uml_class_representation(path_to_header, class_name):
             <td ALIGN="left" BALIGN="left">{}</td>
         </tr>
     </table>>"""
+
     label_value = label_value_template.format(class_caption,
-                                              '<br />'.join(uml_properties_representation),
-                                              '<br />'.join(uml_methods_representation))
+                                              '<br />'.join(html_uml_properties_representation),
+                                              '<br />'.join(html_uml_methods_representation))
     node = node_template.format(label_value)
 
     print node
